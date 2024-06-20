1118ee94091dbdd3fa105ecf2c211f58e9031b4d
Ilka <Ilka@LAPTOP-VAVOB6QC>
2024-06-20 15:13:05 +0200

    w trakcie

---
+++ JsonAddedTopic/Person.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonAddedTopic
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public Person(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+
+        public void SaveToFileAsJson(string filePath)
+        {
+            string json = JsonConvert.SerializeObject(this, Formatting.Indented);
+            File.WriteAllText(filePath, json);
+        }
+    }
+}
--- JsonAddedTopic/Program.cs
+++ JsonAddedTopic/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Json");
+
+            /* 1. Napisz program, który wczytuje plik JSON zawierający informacje o kilku osobach (imię, nazwisko, wiek)
+            i wyświetla te informacje na konsoli */
+            string filePath = @"C:\Users\Ilka\Desktop\.net\people.json";
+            string json = File.ReadAllText(filePath);
+            Console.WriteLine(json);
+
+            /* 2. Stwórz klasę Person z polami FirstName, LastName, Age.
+            Następnie utwórz obiekt tej klasy, wypełnij go danymi i zapisz jako JSON do pliku */
+            Person person = new Person("Mona", "Dana", 20);
+            person.SaveToFileAsJson(filePath);
+            Console.WriteLine("Person saved to file as JSON.");
         }
     }
 }
